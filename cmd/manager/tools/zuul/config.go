6017a9625c7b3e31fe8a1ecaff13332a7d681ae1
xxiao93 <1981467108@qq.com>
2019-12-20 17:24:44 +0800

    Change zuul config file
     * change config file
     * add .gitignore file

--- cmd/manager/tools/zuul/config.go
+++ cmd/manager/tools/zuul/config.go
@@ -8,73 +8,13 @@ var mainyamlTemplate = `
     source:
       gerrit:
         config-projects:
-          - easystack/project-config
+          - xxxxx/project-config
         untrusted-projects:
-          - easystack/zuul-jobs:
-              shadow: easystack/project-config
-          - easystack/easystack-zuul-jobs
-          - easystack/requirements
-          - easystack/neutron
-          - easystack/espresso
-          - easystack/ark
-          - easystack/cube
-          - easystack/ark-infra
-          - easystack/ems-appcenter-dashboard
-          - easystack/rally
-          - easystack/tempest
-          - easystack/charts
-          - easystack/nova
-          - easystack/cinder
-          - easystack/heat
-          - easystack/billing
-          - easystack/ceilometer
-          - easystack/chakra
-          - easystack/gnocchi
-          - easystack/ironic
-          - easystack/manila
-          - easystack/murano
-          - easystack/sahara
-          - easystack/tickets
-          - easystack/trove
-          - easystack/estack-hagent
-          - easystack/keystone
-          - easystack/murano-tempest-plugin
-          - easystack/dr
-          - easystack/python-drclient
-          - easystack/python-muranoclient
-          - easystack/python-billingclient
-          - easystack/python-chakraclient
-          - easystack/python-ticketsclient
-          - easystack/python-ceilometerclient
-          - easystack/python-cinderclient
-          - easystack/python-espressoclient
-          - easystack/python-harborclient
-          - easystack/python-heatclient
-          - easystack/python-keystoneclient
-          - easystack/python-magnumclient
-          - easystack/python-manilaclient
-          - easystack/python-neutronclient
-          - easystack/python-novaclient
-          - easystack/python-openstackclient
-          - easystack/python-troveclient
-          - easystack/python-rollerclient
-          - easystack/ems-servicecatalog-dashboard
-          - easystack/coaster
-          - easystack/roller-dashboard
-          - easystack/keystoneauth
-          - easystack/releases
-          - easystack/alertsaver
-          - easystack/prometheus-openstack-exporter
-          - easystack/eks-dashboard
-          - easystack/escmp-dashboard
-          - easystack/ecs-dashboard
-          - easystack/heat-dashboard
-          - easystack/ems-dashboard
-          - easystack/dr-dashboard
-          - easystack/diamond
-          - easystack/peak
-          - easystack/ESStorage
-          - easystack/django-openstack-auth
+          - xxxxx/zuul-jobs:
+              shadow: xxxxx/project-config
+          - xxxxx/zuul-jobs
+          - xxxxx/requirements
+          - xxxxx/neutron
 `
 
 var zuulschedulerconfigTemplate = `
